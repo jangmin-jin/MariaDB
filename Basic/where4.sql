a73c393f6f3a1b9198e8f4650c8bf8a522fef8da
Jangminjin <jangminjin227@gmail.com>
2021-05-18 17:40:17 +0900

    day_025 210518

--- Basic/where4.sql
+++ Basic/where4.sql
@@ -6,4 +6,4 @@ where sal > (select sal from emp where ename ='BLAKE');
 -- MILLER보다 늦게 입사한 사원의 사번, 이름, 입사일을 출력하시오
 -- select empno, ename, hiredate
 -- from emp
--- where hiredate < (select hiredate from emp where ename = 'MILLER');.
+-- where hiredate < (select hiredate from emp where ename = 'MILLER');
